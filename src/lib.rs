342b7a179b1eb3e7027d568a9beb67c06fd3cbb3
Poacherella <poacherella@gmail.com>
2021-09-18 15:53:00 +0200

    add sounds

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use bevy_kira_audio::{Audio, AudioChannel, AudioPlugin};
 
 use bevy::{
     core::FixedTimestep,
@@ -10,14 +11,16 @@ use wasm_bindgen::prelude::*;
 
 const TIME_STEP: f32 = 1.0 / 60.0;
 // left, right, bottom, top
-const GAME_BOARD: (f32, f32, f32, f32) = (-200.0, 200.0, -350.0, 350.0);
-const GRAVITY_FAC: f32 = 0.07;
+const GAME_BOARD: (f32, f32, f32, f32) = (-200.0, 200.0, -340.0, 330.0);
+const GRAVITY_FAC: f32 = 0.09;
+// const GRAVITY_FAC: f32 = 0.02;
+const UPWARD_FAC: f32 = 0.25;
 
 #[derive(PartialEq, Eq, Hash, Debug, Clone)]
 enum AppState {
+    WarmUp,
     Menu,
     Game,
-    End,
 }
 struct Mine {
     selected: bool,
@@ -28,6 +31,8 @@ struct Mine {
 // Just a marker for the bg
 struct Background;
 struct MainCamera;
+//Marker for the highscoretext
+struct ScoreText;
 
 struct Line;
 
@@ -40,36 +45,47 @@ impl Default for Mine {
         }
     }
 }
+
+#[derive(Debug, Default)]
 struct Player {
     velocity: Vec3,
     maxheight: f32,
     dead: bool,
 }
 
-impl Default for Player {
-    fn default() -> Self {
-        Self {
-            velocity: Vec3::new(0.5, 6.5, 0.0),
-            maxheight: 0.0,
-            dead: false,
-        }
-    }
-}
+// impl Default for Player {
+//     fn default() -> Self {
+//         Self {
+//             velocity: Vec3::default(),
+//             maxheight: 0.0,
+//             dead: false,
+//         }
+//     }
+// }
 
 #[wasm_bindgen]
 pub fn run() {
     let mut app = App::build();
-    app.insert_resource(
-        WindowDescriptor {
-            width: 500.,
-            height: 800.,
-            ..Default::default()
-        });
+    app.insert_resource(WindowDescriptor {
+        width: 500.,
+        height: 800.,
+        ..Default::default()
+    });
+    // app.add_plugin(AudioPlugin);
     app.add_plugins(DefaultPlugins)
+        .add_plugin(AudioPlugin)
         .insert_resource(ClearColor(Color::rgb(0.9, 0.9, 0.9)))
-        // .add_startup_system(setup.system())
-        .add_state(AppState::Game)
-        .add_system_set(SystemSet::on_enter(AppState::Game).with_system(setup.system()))
+        .init_resource::<ButtonMaterials>()
+        .add_state(AppState::WarmUp)
+        .add_system_set(
+            SystemSet::on_enter(AppState::WarmUp)
+                .with_system(setup.system())
+                .with_system(start_music.system()),
+        )
+        .add_system_set(
+            SystemSet::on_enter(AppState::Game)
+                .with_system(init_game.system())
+        )
         .add_system_set(
             SystemSet::on_update(AppState::Game)
                 .with_run_criteria(FixedTimestep::step(TIME_STEP as f64))
@@ -79,10 +95,11 @@ pub fn run() {
                 .with_system(ball_collision_system.system())
                 .with_system(bg_system.system())
                 .with_system(spawn_new_mine_system.system())
-                .with_system(clean_old_mines_system.system())
-            )
-            .add_system_set(
-                SystemSet::on_update(AppState::Game)
+                // .with_system(play_hooked_system.system())
+                .with_system(clean_old_mines_system.system()),
+        )
+        .add_system_set(
+            SystemSet::on_update(AppState::Game)
                 .with_system(mine_movement_system.system())
                 .with_system(player_movement_system.system())
                 .with_system(mine_selector_system.system())
@@ -94,8 +111,12 @@ pub fn run() {
                 .with_system(mine_hook_system.system()),
         )
         .add_system_set(
-            SystemSet::on_exit(AppState::Game).with_system(clean_assets_system.system()),
+            SystemSet::on_exit(AppState::Game)
+                .with_system(end_game_system.system())
+                .with_system(setup_menu.system()),
         )
+        .add_system_set(SystemSet::on_update(AppState::Menu).with_system(menu.system()))
+        .add_system_set(SystemSet::on_exit(AppState::Menu).with_system(cleanup_menu.system()))
         .add_system(scoreboard_system.system())
         .add_system(bevy::input::system::exit_on_esc_system.system());
     // app.add_state(AppState::End);
@@ -105,10 +126,15 @@ pub fn run() {
     app.run();
 }
 
+fn start_music(asset_server: Res<AssetServer>, audio: Res<Audio>) {
+    audio.play_looped(asset_server.load("music.ogg"));
+}
+
 fn setup(
     mut commands: Commands,
     mut materials: ResMut<Assets<ColorMaterial>>,
     asset_server: Res<AssetServer>,
+    mut state: ResMut<State<AppState>>,
 ) {
     // Add the game's entities to our world
 
@@ -117,8 +143,6 @@ fn setup(
         .spawn_bundle(OrthographicCameraBundle::new_2d())
         .insert(MainCamera);
     commands.spawn_bundle(UiCameraBundle::default());
-
-
 
     // bg
     commands
@@ -140,50 +164,54 @@ fn setup(
         })
         .insert(Line);
 
-            // player
+    // player
     commands
-    .spawn_bundle(SpriteBundle {
-        material: materials.add(asset_server.load("player.png").into()),
-        transform: Transform::from_xyz(0.0, -160.0, 1.0),
-        sprite: Sprite::new(Vec2::new(32.0, 32.0)),
-        ..Default::default()
-    })
-    .insert(Player::default());
+        .spawn_bundle(SpriteBundle {
+            material: materials.add(asset_server.load("player.png").into()),
+            sprite: Sprite::new(Vec2::new(32.0, 32.0)),
+            ..Default::default()
+        })
+        .insert(Player::default());
 
     // scoreboard
-    commands.spawn_bundle(TextBundle {
-        text: Text {
-            sections: vec![
-                TextSection {
-                    value: "Score: ".to_string(),
-                    style: TextStyle {
-                        font: asset_server.load("vcr.ttf"),
-                        font_size: 40.0,
-                        color: Color::rgb(0.5, 0.5, 1.0),
+    commands
+        .spawn_bundle(TextBundle {
+            text: Text {
+                sections: vec![
+                    TextSection {
+                        value: "Score: ".to_string(),
+                        style: TextStyle {
+                            font: asset_server.load("vcr.ttf"),
+                            font_size: 40.0,
+                            color: Color::rgb(0.5, 0.5, 1.0),
+                        },
                     },
-                },
-                TextSection {
-                    value: "".to_string(),
-                    style: TextStyle {
-                        font: asset_server.load("vcr.ttf"),
-                        font_size: 40.0,
-                        color: Color::rgb(1.0, 0.5, 0.5),
+                    TextSection {
+                        value: "".to_string(),
+                        style: TextStyle {
+                            font: asset_server.load("vcr.ttf"),
+                            font_size: 40.0,
+                            color: Color::rgb(1.0, 0.5, 0.5),
+                        },
                     },
+                ],
+                ..Default::default()
+            },
+            style: Style {
+                position_type: PositionType::Absolute,
+                position: Rect {
+                    top: Val::Px(5.0),
+                    left: Val::Px(5.0),
+                    ..Default::default()
                 },
-            ],
-            ..Default::default()
-        },
-        style: Style {
-            position_type: PositionType::Absolute,
-            position: Rect {
-                top: Val::Px(5.0),
-                left: Val::Px(5.0),
                 ..Default::default()
             },
             ..Default::default()
-        },
-        ..Default::default()
-    });
+        })
+        .insert(ScoreText);
+
+    // proceed to game
+    state.set(AppState::Game).unwrap();
 }
 
 /// Use the mouse to select a mine
@@ -207,7 +235,7 @@ fn mine_selector_system(
         let p = pos - size / 2.0;
 
         // assuming there is exactly one main camera entity, so this is OK
-        let camera_transform = q_camera.single().unwrap();
+        let camera_transform = q_camera.single().expect("Need exactly one camera");
 
         // apply the camera transform
         let pos_wld = camera_transform.compute_matrix() * p.extend(0.0).extend(1.0);
@@ -248,12 +276,19 @@ fn mine_highlighter_system(
 }
 
 /// Mark a mine hooked
-fn mine_hook_system(btns: Res<Input<MouseButton>>, mut q_mine: Query<&mut Mine>) {
+
+fn mine_hook_system(
+    btns: Res<Input<MouseButton>>,
+    mut q_mine: Query<&mut Mine>,
+    asset_server: Res<AssetServer>,
+    audio: Res<Audio>
+) {
     if btns.just_pressed(MouseButton::Left) {
         // a left click just happened
         for mut m in &mut q_mine.iter_mut() {
             if m.selected {
                 m.hooked = true;
+                audio.play(asset_server.load("sfx100v2_air_02.ogg"));
             } else {
                 m.hooked = false;
             }
@@ -280,7 +315,9 @@ fn spawn_new_mine_system(
         for mine_t in q_mine.iter() {
             highest_mine = highest_mine.max(mine_t.translation.y);
         }
-        if highest_mine - p.maxheight < 100. {
+
+        // if
+        if highest_mine - p.maxheight < 150. {
             let mut rng = rand::thread_rng();
 
             commands
@@ -289,7 +326,7 @@ fn spawn_new_mine_system(
                     sprite: Sprite::new(Vec2::new(32.0, 32.0)),
                     transform: Transform::from_xyz(
                         rng.gen_range(GAME_BOARD.0..GAME_BOARD.1),
-                        rng.gen_range(p.maxheight + 300.0..p.maxheight + 350.),
+                        rng.gen_range(p.maxheight + GAME_BOARD.3..p.maxheight + 450.),
                         1.0,
                     ),
                     ..Default::default()
@@ -363,7 +400,7 @@ fn velocity_towards_mine_system(
         for (mine, m_t) in mine_query.iter() {
             if mine.hooked {
                 let dir = m_t.translation - p_t.translation;
-                player.velocity += dir.normalize() * 0.15;
+                player.velocity += dir.normalize() * UPWARD_FAC;
             }
         }
     }
@@ -405,12 +442,12 @@ fn draw_line_system(
                     let angle = diff.y.atan2(diff.x);
                     l_t.rotation = Quat::from_axis_angle(Vec3::new(0., 0., 1.), angle);
                     any_hooked = true;
-                } 
+                }
             }
         }
         if !any_hooked {
             if let Ok(mut l_t) = q_line.single_mut() {
-                l_t.scale.x = 0.; 
+                l_t.scale.x = 0.;
             }
         }
     }
@@ -450,31 +487,21 @@ fn clean_old_mines_system(
     }
 }
 
-/// clean up everything
-fn clean_assets_system(
-    mut mine_query: Query<Entity, With<Mine>>,
-    player_query: Query<Entity, With<Player>>,
-    mut commands: Commands,
-) {
-    if let Ok(player) = player_query.single() {
-        commands.entity(player).despawn();
-    }
-    for mine in mine_query.iter_mut() {
-        commands.entity(mine).despawn();
-    }
-}
-
 /// Trigger state change
-fn is_player_dead_system(mut app_state: ResMut<State<AppState>>, player_query: Query<&Player>) {
-    if let Ok(player) = player_query.single() {
+fn is_player_dead_system(
+    mut app_state: ResMut<State<AppState>>,
+    mut player_query: Query<&mut Player>,
+) {
+    if let Ok(mut player) = player_query.single_mut() {
         if player.dead {
-            app_state.set(AppState::End).unwrap();
+            player.maxheight = 0.;
+            app_state.set(AppState::Menu).unwrap();
         }
     }
 }
 
 /// update the score
-fn scoreboard_system(mut query: Query<&mut Text>, player_query: Query<&Player>) {
+fn scoreboard_system(mut query: Query<&mut Text, With<ScoreText>>, player_query: Query<&Player>) {
     if let Ok(player) = player_query.single() {
         let mut text = query.single_mut().unwrap();
         text.sections[0].value = format!("Score: {:}", player.maxheight as i32);
@@ -482,13 +509,150 @@ fn scoreboard_system(mut query: Query<&mut Text>, player_query: Query<&Player>) 
 }
 
 /// Very simple wall collision (left/right)
-fn ball_collision_system(mut ball_query: Query<(&mut Player, &Transform)>) {
-    if let Ok((mut player, p_t)) = ball_query.single_mut() {
+fn ball_collision_system(
+    mut ball_query: Query<(&mut Player, &mut Transform)>,
+    asset_server: Res<AssetServer>,
+    audio: Res<Audio>,
+) {
+    if let Ok((mut player, mut p_t)) = ball_query.single_mut() {
         // check collision with walls and "reflect"
-        if p_t.translation.x < GAME_BOARD.0 || p_t.translation.x > GAME_BOARD.1 {
-            player.velocity.x *= -1.;
-            // dampen a bit on impact (seems to get player stuck, perhaps better w/ fixed timestep)
-            // player.velocity *= 0.9;
+        if p_t.translation.x <= GAME_BOARD.0 || p_t.translation.x >= GAME_BOARD.1 {
+            p_t.translation.x = p_t.translation.x.max(GAME_BOARD.0);
+            p_t.translation.x = p_t.translation.x.min(GAME_BOARD.1);
+            // reverse and dampen
+            player.velocity.x *= -0.5;
+            audio.play(asset_server.load("sfx100v2_metal_01.ogg"));
         }
     }
+}
+
+/// TODO: play / end looping sound when hooked, maybe pitch
+fn play_hooked_system(p_query: Query<&Mine>, asset_server: Res<AssetServer>, audio: Res<Audio>) {
+    for m in p_query.iter() {
+        if m.hooked {
+            audio.play(asset_server.load("sfx100v2_air_02.ogg"));
+        }
+    }
+}
+
+/// clean up everything
+fn end_game_system(
+    mut mine_query: Query<Entity, With<Mine>>,
+    player_query: Query<Entity, With<Player>>,
+    cam_query: Query<Entity, With<MainCamera>>,
+    mut commands: Commands,
+) {
+    // if let Ok(player) = player_query.single() {
+    //     commands.entity(player).despawn();
+    // }
+    for mine in mine_query.iter_mut() {
+        commands.entity(mine).despawn();
+    }
+    // if let Ok(cam) = cam_query.single() {
+    //     commands.entity(cam).despawn();
+    // }
+}
+
+/// clean up everything
+fn init_game(
+    mut player_query: Query<(&mut Player, &mut Transform)>,
+) {
+    if let Ok((mut player, mut transform)) = player_query.single_mut() {
+        *transform = Transform::from_xyz(0.0, -160.0, 1.0);
+        player.velocity = Vec3::new(0.5, 15.5, 0.0);
+        player.maxheight = 0.0;
+        player.dead = false;
+    }
+    // if let Ok(cam) = cam_query.single() {
+    //     commands.entity(cam).despawn();
+    // }
+}
+
+struct ButtonMaterials {
+    normal: Handle<ColorMaterial>,
+    hovered: Handle<ColorMaterial>,
+    pressed: Handle<ColorMaterial>,
+}
+
+fn setup_menu(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    button_materials: Res<ButtonMaterials>,
+) {
+    // ui camera
+    // commands.spawn_bundle(UiCameraBundle::default());
+    let button_entity = commands
+        .spawn_bundle(ButtonBundle {
+            style: Style {
+                size: Size::new(Val::Px(150.0), Val::Px(65.0)),
+                // center button
+                margin: Rect::all(Val::Auto),
+                // horizontally center child text
+                justify_content: JustifyContent::Center,
+                // vertically center child text
+                align_items: AlignItems::Center,
+                ..Default::default()
+            },
+            material: button_materials.normal.clone(),
+            ..Default::default()
+        })
+        .with_children(|parent| {
+            parent.spawn_bundle(TextBundle {
+                text: Text::with_section(
+                    "Play",
+                    TextStyle {
+                        font: asset_server.load("vcr.ttf"),
+                        font_size: 40.0,
+                        color: Color::rgb(0.9, 0.9, 0.9),
+                    },
+                    Default::default(),
+                ),
+                ..Default::default()
+            });
+        })
+        .id();
+    commands.insert_resource(MenuData { button_entity });
+}
+
+impl FromWorld for ButtonMaterials {
+    fn from_world(world: &mut World) -> Self {
+        let mut materials = world.get_resource_mut::<Assets<ColorMaterial>>().unwrap();
+        ButtonMaterials {
+            normal: materials.add(Color::rgb(0.15, 0.15, 0.15).into()),
+            hovered: materials.add(Color::rgb(0.25, 0.25, 0.25).into()),
+            pressed: materials.add(Color::rgb(0.35, 0.75, 0.35).into()),
+        }
+    }
+}
+
+/// Despawn all menu items
+fn cleanup_menu(mut commands: Commands, menu_data: Res<MenuData>) {
+    commands.entity(menu_data.button_entity).despawn_recursive();
+}
+
+fn menu(
+    mut state: ResMut<State<AppState>>,
+    button_materials: Res<ButtonMaterials>,
+    mut interaction_query: Query<
+        (&Interaction, &mut Handle<ColorMaterial>),
+        (Changed<Interaction>, With<Button>),
+    >,
+) {
+    for (interaction, mut material) in interaction_query.iter_mut() {
+        match *interaction {
+            Interaction::Clicked => {
+                *material = button_materials.pressed.clone();
+                state.set(AppState::Game).unwrap();
+            }
+            Interaction::Hovered => {
+                *material = button_materials.hovered.clone();
+            }
+            Interaction::None => {
+                *material = button_materials.normal.clone();
+            }
+        }
+    }
+}
+struct MenuData {
+    button_entity: Entity,
 }
